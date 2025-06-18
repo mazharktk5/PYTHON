847da3b87672938d8669f03934a2a746dff86d75
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-06-18 09:59:43 +0500

    learned variable names

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,3 @@
 print("Hello world")
 print("*" * 10)
+print("hello")
---
+++ Variables.py
@@ -0,0 +1,11 @@
+students = 2000
+teachers = 100
+institution = "ABC University"
+rating = 4.5
+isGovernmentFunded = True
+print("Number of students:", students)
+print("Number of teachers:", teachers)
+print("Institution name:", institution)
+print("Rating:", rating)
+print("Is government funded:", isGovernmentFunded)
+
