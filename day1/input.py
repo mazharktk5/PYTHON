db0fd605ee480ea9860a55e4ddae1bb33a5fd7ba
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-06 13:42:12 +0500

    learned input

---
+++ day1/input.py
@@ -0,0 +1,16 @@
+
+
+num = input("Enter a number:")
+
+print(num)
+print(type(num))
+
+name = input("enter your name: ")
+print(name)
+print(type(name))
+
+num2 =int(input("Enter a number:"))
+print(num2)
+print(type(num2))
+
+
