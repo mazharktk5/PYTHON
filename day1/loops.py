887716cf203409c171de9df24d4d11685cec1b22
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-06 15:26:42 +0500

    learned loops

---
+++ day1/loops.py
@@ -0,0 +1,34 @@
+# languages = ["Python", "Java", "C++", "JavaScript"]
+
+# # for i in languages:
+# #     print(i)
+
+# for i in range(0, 2):
+#     print(languages[i])
+
+
+# languages = "python"
+# for i in languages:
+#     print(i)
+
+# for i in range(0, 10):
+#     if i % 3 == 0 and i != 0:
+#         print("Condition fulfilled at:", i)
+#         break
+#     # print(i)
+
+
+# languages = ["Python", "Java", "C++", "JavaScript"]
+# for language in languages:
+#     if language == "C++":
+#         print("C++ found, skipping it")
+#         continue
+#     print(language)
+
+# cars = ["BMW", "Mercedes", "Audi", "Toyota"]
+# company = ["BMW AG", "Daimler AG", "Volkswagen AG", "Toyota Motor Corporation"]
+
+# for car in cars:
+#     for comp in company:
+#         print(f"{car} is manufactured by {comp}")
+#     print("-----\n-----")
