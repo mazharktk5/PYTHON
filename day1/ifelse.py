2ff2e8c8cdacaf7ed3f111849cfac32447936b87
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-06 14:48:17 +0500

    learned if else statements

---
+++ day1/ifelse.py
@@ -0,0 +1,42 @@
+
+# num = 0
+
+# if num > 10:
+#     print("num is greater than 10")
+
+
+# else:
+#     print("num is less than or equal to 10")
+
+
+# num = int(input("Enter a number: "))
+# result = 0
+
+# if num != 0:
+#     result += num
+#     print("result is", result)
+# else:
+#     print("num is zero, result remains", result)
+
+# num = int(input("Enter a number: "))
+
+# if num > 0:
+#     print("num is positive")
+# elif num < 0:
+#     print("num is negative")
+# else:
+#     print("num is zero")
+
+
+# num = int(input("Enter a number: "))
+# # -5
+
+
+# if num >= 0:
+#     if num == 0:
+#         print("num is zero")
+#     else:
+#         print("num is positive")
+# else:
+#     print("num is negative")
+
