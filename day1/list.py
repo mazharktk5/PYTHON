cf53770bd3810d664eda33f518b17f0d8d220100
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-07 12:12:37 +0500

    learned lists

---
+++ day1/list.py
@@ -0,0 +1,53 @@
+# mylist = [2, 4, 6, 8, 10]
+
+# # print("List:", mylist)
+# # print("Type:", type(mylist))
+
+# list2 = ["apple", "banana", "cherry", "date"]
+# # print("List2:", list2)
+
+# print(list2[-1])
+# print(mylist[0])
+# print(mylist[1:3])  # Slicing the list
+
+# thislist = ['alice', 'bob', 'charlie', 'dave']
+
+# if 'bob' in thislist:
+#     print("Bob is in the list.")
+
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1] = "blackcurrant"
+# print(thislist)
+
+# thislist = ["apple", "banana", "cherry"]
+# thislist[1:3] = ["watermelon"]
+# print(thislist)
+
+myList = [3,5,6,4,2,7]
+# myList.insert(2,8)
+# print("List after insertion:", myList)
+
+# myList.append(9)
+# print("List after appending:", myList)
+
+# list1 =["karachi", "lahore", "islamabad"]
+# list2 = ["peshawar", "quetta", "multan"]
+
+# list1.extend(list2)
+# print("Extended List:", list1)
+
+
+list = [1, 2, 3, 4, 5]
+# list.remove(3)
+# print("List after removing 3:", list)
+# list.pop()
+# print("List after popping last element:", list)
+# list.pop(2)
+# print("List after popping element at index 2:", list)
+
+del list[1]
+print("List after deleting element at index 1:", list)
+
+list.clear()
+print("List after clearing all elements:", list)
