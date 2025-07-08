88ecc18b8ef5d5a12ecd89070cc9d8920b33686b
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-08 14:03:51 +0500

    strings and lists

---
+++ day2/lists.py
@@ -0,0 +1,18 @@
+list =["Mazhar","Ali",2,4,5,True,False,3.14]
+print(list)
+print(list[0])
+print(list[6])
+
+list[3] = 10
+print(list)
+
+list.append("ahmad")
+print(list)
+list.insert(2, "inserted")
+print(list)
+list.remove("inserted")
+print(list)
+list.pop(4)
+print(list)
+list.sort()
+print(list)
---
+++ day2/strings.py
@@ -0,0 +1,18 @@
+
+# name = input("enter your name: ")
+
+# print(f"Good afternoon, {name}!")
+
+# str = "My name is  Mazhar and  I am a web developer"
+
+# print(str.find("  "))
+# print(str.replace("  ", " "))
+# print(str)
+# print(str.split(" "))
+# print(str.split(" ", 2))
+# print(str.split(" ", 1))
+# print(str.split(" ", 3))
+
+str = "My name is  Mazhar.\n\tAnd I am a web developer"
+print(str)
+
