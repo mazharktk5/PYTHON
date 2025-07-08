0a134b76dda5b809c554d41ff413823dc0e1c5fb
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-08 14:35:31 +0500

    practiced

--- day2/lists.py
+++ day2/lists.py
@@ -1,18 +1,44 @@
-list =["Mazhar","Ali",2,4,5,True,False,3.14]
-print(list)
-print(list[0])
-print(list[6])
+# list =["Mazhar","Ali",2,4,5,True,False,3.14]
+# print(list)
+# print(list[0])
+# print(list[6])
 
-list[3] = 10
-print(list)
+# list[3] = 10
+# print(list)
 
-list.append("ahmad")
-print(list)
-list.insert(2, "inserted")
-print(list)
-list.remove("inserted")
-print(list)
-list.pop(4)
-print(list)
-list.sort()
-print(list)
+# list.append("ahmad")
+# print(list)
+# list.insert(2, "inserted")
+# print(list)
+# list.remove("inserted")
+# print(list)
+# list.pop(4)
+# print(list)
+# list.sort()
+# print(list)
+
+# fruits =[]
+# fruit1 = input("Enter first fruit: ")
+# fruits.append(fruit1)
+# fruit2 = input("Enter second fruit: ")
+# fruits.append(fruit2)
+# fruit3 = input("Enter third fruit: ")
+# fruits.append(fruit3)
+ 
+# fruit4 = input("Enter fourth fruit: ")
+# fruits.append(fruit4)
+# fruit5 = input("Enter fifth fruit: ")
+# fruits.append(fruit5)
+# fruit6 = input("Enter sixth fruit: ")
+# fruits.append(fruit6)
+# fruit7 = input("Enter seventh fruit: ")
+# fruits.append(fruit7)
+# print(fruits)
+
+marks = []
+for i in range(5):
+    mark = int(input(f"Enter mark for student {i+1}: "))
+    marks.append(mark)
+    marks.sort()
+print("Marks of students:", marks)
+
---
+++ day2/tuples.py
@@ -0,0 +1,18 @@
+# tuple = ("Mazhar", "Ali", 2, 4,2, 5, True, False, 3.14)
+# print(tuple)
+# print(tuple[0])
+# print(tuple[6])
+
+# print(tuple.count(2))
+
+# print(tuple.index(3.14))
+
+
+numbers = (1, 2, 3, 4, 5)
+print(sum(numbers))  # Output: 15
+print(max(numbers))  # Output: 5
+print(min(numbers))  # Output: 1
+
+n = (0,4,5,6,3,6,5,6,7,6,4,3,6)
+print(n.count(6))  # Output: 5
+print(n.index(6))  # Output: 3 (the first occurrence of 6
