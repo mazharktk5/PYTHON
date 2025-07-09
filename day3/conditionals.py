8b8632bcaca26556672a98d92b15a083cea764b4
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-09 22:25:36 +0500

    practice

--- day3/conditionals.py
+++ day3/conditionals.py
@@ -1,11 +1,27 @@
 # 
 
-p1 = "buy now"
-p2 = "click this"
+# p1 = "buy now"
+# p2 = "click this"
 
-comment = input("Enter your comment: ")
+# comment = input("Enter your comment: ")
 
-if (p1 in comment) or (p2 in comment):
-    print("You are a spammer!")
+# if (p1 in comment) or (p2 in comment):
+#     print("You are a spammer!")
+# else:
+#     print("Thank you for your comment!")
+
+# name = input("Enter your name: ")
+
+# if(len(name) < 10):
+#     print("Your name is short")
+# else:
+#     print("Your name is long")
+
+
+
+list = ["mazhar", "ali", "khan"]
+
+if("mazhar" in list):
+    print("mazhar is in the list")
 else:
-    print("Thank you for your comment!")
+    print("mazhar is not in the list")
