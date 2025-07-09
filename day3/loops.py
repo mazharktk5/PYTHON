5c94c21edb3077e381d98d8f3c1555f872e3d55d
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-09 23:21:56 +0500

    practiced

--- day3/loops.py
+++ day3/loops.py
@@ -1,5 +1,14 @@
-i = 0
+# i = 0
 
-while i < 5:
-    print(f"Hello{i}")
-    i += 1
+# while i < 5:
+#     print(f"Hello{i}")
+#     i += 1
+
+# list = ["mazhar", "ali", "khan",3 ,4, 5, 6, 7, 8, 9, 10]
+
+
+# i = 0
+# while i < len(list):
+#     print(f"Hello {list[i]}")
+#     i += 1
+
