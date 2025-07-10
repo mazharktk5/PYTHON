8d65bfd6d13d7bfe9e58cf93caf564f85a500469
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-10 16:22:52 +0500

    files

---
+++ day4/files.py
@@ -0,0 +1,14 @@
+
+
+# f = open("file.txt")
+# data = f.read()
+# print(data)
+# f.close()
+
+str = "my name is mazhar ahmad and i am mern stack developer"
+
+f = open("myfile.txt","w")
+
+f.write(str)
+
+f.close
