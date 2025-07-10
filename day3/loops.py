952a4e5c41a3dde81c17afad596515c31615173a
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-10 15:46:01 +0500

    pattern

--- day3/loops.py
+++ day3/loops.py
@@ -91,6 +91,7 @@
 # for i in range(1, num + 1):
 #     print(" " * (num - i) + "*" * (2 * i - 1))
 
+
 # num = int(input("Enter a number: "))
 # for i in range(1,num + 1):
 #     print("*" * i)
--- day4/functions.py
+++ day4/functions.py
@@ -9,7 +9,6 @@
 # avg()
 
 
-
 # def Greeting(name):
 #     print(f"Hello {name}, welcome to the Python course!")
 
@@ -18,14 +17,12 @@
 # def factorial(n):
 #     if(n==1 or n == 0):
 #         return 1
-    
+
 #     return n* factorial(n-1)
 
-    
 
 # Factorial = factorial(4)
 # print(Factorial)
-
 
 
 # def greatest(a,b,c):
@@ -38,9 +35,29 @@
 
 # print(greatest(3,9,6))
 
+
 # def f_to_c(f):
 #     return 5 * (f-32)/9
 
 # f = int(input("Enter temperature in F....: "))
 
 # print(round(f_to_c(f),2))
+
+
+# def sum(n):
+#     if(n==1):
+#         return 1
+#     return sum(n-1) + n
+
+# print(sum(3))
+
+
+def pattern(n):
+    if (n == 0):
+        return
+
+    print("*" * n)
+    pattern(n-1)
+
+
+pattern(5)
