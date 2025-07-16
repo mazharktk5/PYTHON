f0be3f678b0657aeddaa00358c27cd3e6ecd720d
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-16 15:20:58 +0500

    updted

--- day5/class.py
+++ day5/class.py
@@ -24,17 +24,33 @@
 
 
 
-class Programer:
-    def __init__(self, name, age, salary):
+# class Programer:
+#     def __init__(self, name, age, salary):
+#         self.name = name
+#         self.age = age
+#         self.salary = salary
+
+
+# p1 = Programer("Mazhar",20,230004)
+# print(p1.name,p1.age,p1.salary)
+# p2 = Programer("ALi" , 23  ,23400)
+# print(p2.name,p2.age,p2.salary)  
+
+
+
+class Train:
+    def __init__(self, name, speed, year):
         self.name = name
-        self.age = age
-        self.salary = salary
+        self.speed = speed
+        self.year = year
+        def displayInfo(self):
+            print(f"Name: {self.name}")
+            print(f"Speed: {self.speed} km/h")
+            print(f"Year: {self.year}")
+            displayInfo(self)
+            print("Train info displayed")
 
-
-p1 = Programer("Mazhar",20,230004)
-print(p1.name,p1.age,p1.salary)
-p2 = Programer("ALi" , 23  ,23400)
-print(p2.name,p2.age,p2.salary)  
+displayInfo(self)
 
         
         
