ea3966948c52c0eb45386014273be83550b1ff67
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-16 20:47:02 +0500

    multiple inheritance

---
+++ day5/multiplle-inheritacne.py
@@ -0,0 +1,21 @@
+
+class Father:
+    def speak(self):
+        print("Father speaks.")
+
+
+class Mother:
+    def cook(self):
+        print("Mother cooks.")
+
+
+class Child(Father, Mother):
+    def play(self):
+        print("child play")
+
+
+c = Child()
+
+c.speak()
+c.cook()
+c.play()
