c804f7746cd631ba3c311a9cb13a5c2125f39904
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-07-16 15:17:10 +0500

    updated

---
+++ day5/class.py
@@ -0,0 +1,41 @@
+
+
+# class Student:
+#     name = "Mazhar"
+#     age = 20
+
+
+# s = Student()
+
+# s.salary = 20000
+
+# print(s.name, s.salary)
+
+
+# class Company:
+#     name = "MAzhar group "
+#     location = "Peshawer"
+#     def getInfo(self):
+#         print({self.location})
+
+
+# c = Company()
+# c.getInfo()  # Output: {'Peshawer'}
+
+
+
+class Programer:
+    def __init__(self, name, age, salary):
+        self.name = name
+        self.age = age
+        self.salary = salary
+
+
+p1 = Programer("Mazhar",20,230004)
+print(p1.name,p1.age,p1.salary)
+p2 = Programer("ALi" , 23  ,23400)
+print(p2.name,p2.age,p2.salary)  # Output: Mazhar 20
+
+        
+        
+
